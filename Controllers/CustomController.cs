3c15d47d4378910895549a84ab883164cad7b722
Pavithraramesh19 <pavithraramesh1902@gmail.com>
2024-02-01 17:54:00 +0530

    changes

---
+++ Controllers/CustomController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_2_MVC.Controllers
+{
+    public class CustomController : Controller
+    {
+        // GET: Custom
+        public ActionResult Index()
+        {
+            ViewBag.Msg = "These are the Customers";
+            
+            List<string> list = new List<string>
+            {
+             "Robert",
+            "George",
+            "Anabell",
+            "Nitin",
+            "Deepak"
+            };
+            ViewBag.nameList = list;
+            return View();
+        }
+        public ActionResult RedirectDemo()
+        {
+            TempData["displaymsg"] = "Thanks for visiting us!!!";
+            return View();
+        }
+    }
+}
